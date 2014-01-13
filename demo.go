6d06e0b90312ecee87af1902139ab2d08a2ff7a2
Nikolay Georgiev <master033@gmail.com>
2014-01-14 00:59:01 +0200

    Added authentication to the demo.

--- demo.go
+++ demo.go
@@ -7,10 +7,11 @@ import (
 
 func main() {
 	sharky := new(goSharky.GoSharky)
+	sharky.Authenticate("username", md5("password"))
 	songs := sharky.GetPopularSongMonth(10)
 	country := sharky.GetCountry("17.235.72.103")
 	streamDetails := GetStreamKeyStreamServer(songs[0].id, country, false)
 
 	mp3player := new(3rdPartyMp3Player)
-	mp3player.play(streamDetails.stream)
+	mp3player.play(streamDetails.songStreamUrl)
 }
